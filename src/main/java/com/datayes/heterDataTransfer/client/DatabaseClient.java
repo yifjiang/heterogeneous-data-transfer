e47403e9c97a3f02d5275e51b166b2e2d0fc0147
Zhaojie Gong <gzjmail1995@gmail.com>
2018-06-12 07:34:23 -0700

    kafka examples

---
+++ src/main/java/com/datayes/heterDataTransfer/client/DatabaseClient.java
@@ -0,0 +1,46 @@
+package com.datayes.heterDataTransfer.client;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class DatabaseClient {
+
+    public static void main(String[] args) {
+
+        //Kafka consumer configuration settings
+        String topicName = "Changes";
+        Properties props = new Properties();
+
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer
+                <String, String>(props);
+
+        //Kafka Consumer subscribes list of topics here.
+        consumer.subscribe(Arrays.asList(topicName));
+
+        //print the topic name
+        System.out.println("Subscribed to topic " + topicName);
+        int i = 0;
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+
+                // print the offset,key and value for the consumer records.
+                System.out.printf("partition = %d, offset = %d, key = %s, value = %s\n",
+                        record.partition(), record.offset(), record.key(), record.value());
+        }
+    }
+}
