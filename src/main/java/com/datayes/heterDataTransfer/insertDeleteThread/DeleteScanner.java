2a2a32e7aedb2ab2d15a56f29c007849d98e5a1f
Junlin95 <38867676+Junlin95@users.noreply.github.com>
2018-06-21 11:36:00 +0800

    delete scanner with partition

--- src/main/java/com/datayes/heterDataTransfer/insertDeleteThread/DeleteScanner.java
+++ src/main/java/com/datayes/heterDataTransfer/insertDeleteThread/DeleteScanner.java
@@ -30,46 +30,56 @@ public class DeleteScanner extends Thread{
                     readFile.createNewFile();
                 }
 
-                List<Long> preIdSet = readIdList(readFile);
-                List<Long> curIdSet = new ArrayList<>();
-
+                //Execute the query
                 Statement stmt = con.createStatement();
                 stmt.setFetchSize(fetchSize);
                 ResultSet rst = stmt.executeQuery("SELECT ID FROM " + currentTable);
+                StringBuilder content = new StringBuilder();
 
-                while(rst.next()){
-                    curIdSet.add(rst.getLong("ID"));
-                }
-
+                //Read by partition
+                int start = 0;
+                List<Long> preIdSet = new ArrayList<>();
+                List<Long> curIdSet = new ArrayList<>();
                 int prePtr = 0;
                 int curPtr = 0;
-
-                while (prePtr < preIdSet.size() && curPtr < curIdSet.size()){
-                    long curId = curIdSet.get(curPtr);
-                    long preId = preIdSet.get(prePtr);
-                    if (curId > preId){
-                        System.out.println("new delete id: " + preId);
-                        prePtr += 1;
-                    } else if (curId == preId){
-                        prePtr += 1;
-                        curPtr += 1;
-                    } else{
-                        curPtr += 1;
+                while (true){
+                    if (prePtr >= preIdSet.size()){
+                        preIdSet = readIdListByPartition(readFile, start, fetchSize);
+                        start += fetchSize;
+                        prePtr = 0;
+                    }
+                    if (curPtr >= curIdSet.size()){
+                        curIdSet = readDataBaseByPartition(rst, fetchSize);
+                        content = fileWriter(content, curIdSet);
+                        curPtr = 0;
+                    }
+                    while (prePtr < preIdSet.size() && curPtr < curIdSet.size()){
+                        long curId = curIdSet.get(curPtr);
+                        long preId = preIdSet.get(prePtr);
+                        if (curId > preId){
+                            System.out.println("new delete id: " + preId);
+                            prePtr += 1;
+                        } else if (curId == preId){
+                            prePtr += 1;
+                            curPtr += 1;
+                        } else{
+                            curPtr += 1;
+                        }
+                    }
+                    if (curIdSet.size() == 0 || preIdSet.size() == 0){
+                        while (prePtr < preIdSet.size()){
+                            System.out.println("new delete id: " + preIdSet.get(prePtr));
+                            prePtr += 1;
+                        }
+                        while(curIdSet.size() > 0){
+                            curIdSet = readDataBaseByPartition(rst, fetchSize);
+                            content = fileWriter(content, curIdSet);
+                        }
+                        break;
                     }
                 }
-                while (prePtr < preIdSet.size()){
-                    long preId = preIdSet.get(prePtr);
-                    System.out.println("new delete id: " + preId);
-                    prePtr += 1;
-                }
-
                 BufferedWriter out = new BufferedWriter(new FileWriter("./backup.txt", false));
-                StringBuilder content = new StringBuilder();
-                //System.out.println("write:");
-                //System.out.println(curIdSet);
-                for (int i = 0; i < curIdSet.size(); i += 1){
-                    content.append(String.valueOf(curIdSet.get(i)) + "\n");
-                }
+
                 out.write(content.toString());
                 out.close();
 
@@ -85,16 +95,38 @@ public class DeleteScanner extends Thread{
 
     }
 
-    private static List<Long> readIdList(File fin) throws IOException {
+    private static List<Long> readIdListByPartition(File fin, int start, int size) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(fin));
         List<Long> result = new ArrayList<>();
+        while (start-- > 0){
+            br.readLine();
+        }
         String line;
-        while ((line = br.readLine()) != null) {
+        while ((line = br.readLine()) != null && size-- > 0) {
             result.add(Long.parseLong(line));
         }
         br.close();
         return result;
     }
+
+    private static List<Long> readDataBaseByPartition(ResultSet rst, int size) throws SQLException {
+        List<Long> result = new ArrayList<>();
+        for (int i = 0; i < size; i += 1){
+            if(rst.next()){
+                result.add(rst.getLong("ID"));
+            }
+        }
+        return result;
+    }
+
+    private static StringBuilder fileWriter(StringBuilder content, List<Long> curIdSet){
+        for (int i = 0; i < curIdSet.size(); i += 1){
+            content.append(String.valueOf(curIdSet.get(i)) + "\n");
+        }
+        return content;
+    }
+
+
 
 
 }
