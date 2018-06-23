22ed8d0b14c07096321bf240f1aca456b4d03428
lyhdk7 <lyhdk7@outlook.com>
2018-06-24 02:01:37 +0800

    Add kafka sender and send simple increament msg including ID field and TMSTAMP field. I will add more field in next version. Can not run test case which change the ID number. In real business environment, what we update is not ID field, so it's ok.

--- src/main/java/com/datayes/heterDataTransfer/insertDeleteThread/Config.java
+++ src/main/java/com/datayes/heterDataTransfer/insertDeleteThread/Config.java
@@ -10,4 +10,6 @@ public class Config {
     static String sqlClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     static String hostAddress = "localhost";
     static int hostPort = 8001;
+
+    static String topicName = "Changes";
 }
