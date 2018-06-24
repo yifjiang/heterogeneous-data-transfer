04113ddf694b289b33b72445d1cbcf8dd40d9a5e
yifjiang <yifjiang@umich.edu>
2018-06-24 19:14:18 +0800

    package

--- src/main/java/com/datayes/heterDataTransfer/insertDeleteThread/Config.java
+++ src/main/java/com/datayes/heterDataTransfer/insertDeleteThread/Config.java
@@ -7,6 +7,9 @@ public class Config {
     static String sqlConnectionUrl = "jdbc:sqlserver://localhost:1433;" +
             "databaseName=testDB;" +
             "user=sa;password=Vm450Group7;";
+//    static String sqlConnectionUrl = "jdbc:sqlserver://localhost:1433;" +
+//            "databaseName=testMSSQL;" +
+//            "user=sa;password=reallyStrongPwd123;";
     static String sqlClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     static String hostAddress = "localhost";
     static int hostPort = 8001;
